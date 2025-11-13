1d2f7d5f5eea10eaf890ba32d6cfc02d1f59a689
adam.krupa <adam.e.krupa@gmail.com>
2025-11-13 20:39:20 +0100

    added PR pipeline

--- src/env.d.ts
+++ src/env.d.ts
@@ -12,7 +12,6 @@ interface ImportMetaEnv {
   readonly SUPABASE_KEY: string;
   readonly SUPABASE_PUBLIC_KEY: string;
   readonly OPENROUTER_API_KEY: string;
-  readonly E2E_USERNAME_ID: string;
   readonly E2E_USERNAME: string;
   readonly E2E_PASSWORD: string;
 }
